4d076005e3251f568067f6a2bd50703c7b9d6f18
Eduardo Fajardo <efmarcial1060@gmail.com>
2022-04-24 11:09:29 -0400

    test

--- python_django_restaurant_website_project/settings.py
+++ python_django_restaurant_website_project/settings.py
@@ -11,11 +11,13 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import os
+import mimetypes
 
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+mimetypes.add_type("text/css", ".css", True)
 
 
 # Quick-start development settings - unsuitable for production
